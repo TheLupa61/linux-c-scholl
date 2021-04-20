20f87ac16446508b3ae8bc32375fc8ef5647c0db
TheLupa <at-1-97@mail.ru>
2021-04-20 01:30:16 -0700

    home2

--- school21/libft/libft.h
+++ school21/libft/libft.h
@@ -16,8 +16,6 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int		ft_strlen(const char *s);
-int		ft_atoi(const char *str);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
@@ -25,10 +23,21 @@ void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr( const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+int		ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+
+int		ft_atoi(const char *str);
+
+int     ft_isalpha(int c);
+int     ft_isdigit(int c);
+int     ft_isascii(int c);
+int     ft_isprint(int c);
+int     ft_toupper(int c);
+int     ft_tolower(int c);
 #endif
