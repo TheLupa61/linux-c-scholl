be0fc2fe59e038998fa0aa1038b557828c60ed39
TheLupa <at-1-97@mail.ru>
2021-04-22 14:15:57 -0700

    home5

--- school21/libft/ft_split.c
+++ school21/libft/ft_split.c
@@ -1,116 +1,68 @@
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
 
-#include "libft.h"
-
-static char	**ft_malloc_error(char **tab)
+static int  ft_nb_s(const char *s, char c)
 {
-	unsigned int	i;
+    size_t i;
 
-	i = 0;
-	while (tab[i])
-	{
-		free(tab[i]);
-		i++;
-	}
-	free(tab);
-	return (NULL);
-}
-
-static unsigned int	ft_get_nb_strs(char const *s, char c)
-{
-	unsigned int	i;
-	unsigned int	nb_strs;
-
-	if (!s[0])
-		return (0);
-	i = 0;
-	nb_strs = 0;
-	while (s[i] && s[i] == c)
-		i++;
-	while (s[i])
-	{
-		if (s[i] == c)
-		{
-			nb_strs++;
-			while (s[i] && s[i] == c)
-				i++;
-			continue ;
-		}
-		i++;
-	}
-	if (s[i - 1] != c)
-		nb_strs++;
-	return (nb_strs);
-}
-
-static void	ft_get_next_str(char **next_str, unsigned int *next_str_len, char c)
-{
-	unsigned int	i;
-
-	*next_str += *next_str_len;
-	*next_str_len = 0;
-	i = 0;
-	while (**next_str && **next_str == c)
-		(*next_str)++;
-	while ((*next_str)[i])
-	{
-		if ((*next_str)[i] == c)
-			return ;
-		(*next_str_len)++;
-		i++;
-	}
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char			**tab;
-	char			*next_str;
-	unsigned int	next_str_len;
-	unsigned int	nb_strs;
-	unsigned int	i;
-
-	if (!s)
-		return (NULL);
-	nb_strs = ft_get_nb_strs(s, c);
-	if (!(tab = (char **)malloc(sizeof(char *) * (nb_strs + 1))))
-		return (NULL);
-	i = 0;
-	next_str = (char *)s;
-	next_str_len = 0;
-	while (i < nb_strs)
-	{
-		ft_get_next_str(&next_str, &next_str_len, c);
-		if (!(tab[i] = (char *)malloc(sizeof(char) * (next_str_len + 1))))
-			return (ft_malloc_error(tab));
-		ft_strlcpy(tab[i], next_str, next_str_len + 1);
-		i++;
-	}
-	tab[i] = NULL;
-	return (tab);
-}
-
-int main()
-{
-    char months[] = "   lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse   ";
-    char** tokens;
-
-    printf("months=[%s]\n\n", months);
-
-    tokens = ft_split(months, ' ');
-
-    if (tokens)
+    i = 0;
+    while (*s)
     {
-        int i;
-        for (i = 0; *(tokens + i); i++)
+        while (*s == c)
+            s++;
+        if (*s)
         {
-            printf("month=[%s]\n", *(tokens + i));
-            free(*(tokens + i));
+            i++;
+            while (*s && *s != c)
+                s++;
         }
-        printf("\n");
-        free(tokens);
     }
+    return (i);
+}
 
-    return 0;
+static void *ft_next_c(char const **s, char c)
+{
+    char *str;
+    size_t i;
+
+    i = 0;
+    while ((*s)[i] && (*s)[i] != c)
+        i++;
+    str = ft_substr(*s, 0, i);
+    if (str == NULL)
+        return (NULL);
+    *s += i;
+    return(str);
+}
+
+static void  *ft_error(char **new_str, size_t t)
+{
+    while (t--)
+        free(new_str[t]);
+    free(new_str);
+    return (NULL);
+}
+
+char **ft_split(char const *s, char c)
+{
+    char **new_s;
+    size_t i;
+
+    new_s = (char **)malloc(sizeof(char *) * (ft_nb_s(s, c) + 1));
+    if (new_s == NULL)
+        return (NULL);
+    i = 0;
+    while (*s)
+    {
+        while (*s == c)
+            s++;
+        if (*s)
+        {
+            new_s[i] = ft_next_c(&s, c);
+            if (new_s[i] == NULL)
+                return (ft_error(new_s, i));
+            i++;
+        }
+    }
+    new_s[i] = NULL;
+    return (new_s);
 }
