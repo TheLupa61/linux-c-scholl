a823d6f34c0e14fc610003125c75142c57ccdccd
Constance Corrin <ccorrin@vo-j5.msk.21-school.ru>
2021-05-02 19:03:19 +0300

    Done445

--- school21/libft2/ft_substr.c
+++ school21/libft2/ft_substr.c
@@ -4,15 +4,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*str;
 	unsigned int	i;
+	size_t			new_len;
 
-	i = 0;
 	if (!s)
 		return (NULL);
 	if (start >= (unsigned int)ft_strlen(s))
 		return (ft_strdup(""));
+	new_len = ft_strlen(s) - start;
+	if (len > new_len)
+		len = new_len;
 	str = (char *)malloc(sizeof(char) * len + 1);
 	if (!str)
 		return (NULL);
+	i = 0;
 	while (i < len && s[start])
 		str[i++] = s[start++];
 	str[i] = '\0';
